327444982bda873f7a08eae177ebdafcf2d10c5f
perrywang <perrywangsh@gmail.com>
2015-12-12 16:34:35 +0800

    add relation for Question model

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function answers()
+    {
+    	return $this->hasMany('App\Answer');
+    }
+
+    public function submitter()
+    {
+    	return $this->belongsTo('App\User','submit_by');
+    }
 }
